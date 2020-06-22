855df2b93651b5b7a086c80f2b4610fe77ef5b16
UNathi Khoba <ukhoba@c4r10s1.wethinkcode.co.za>
2020-06-22 16:55:40 +0200

    ex07 completed

---
+++ ex07/ft_strrev.c
@@ -0,0 +1,27 @@
+char	*ft_strrev(char *str)
+{
+	int		end;
+	int		start;
+	char	tmp;
+	
+	end = 0;
+
+	while	(str[end] != '\0')
+	{
+		end++;
+	}
+
+	start = 0;
+
+	while(start <= end - 1){
+
+		tmp = *(str + start);
+		str[start] = *(str + (end - 1));
+		str[end] = tmp;
+
+		start++;
+		end--;
+	}
+
+	return str;
+}
